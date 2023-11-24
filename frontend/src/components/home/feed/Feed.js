2064c325e5a5c95a89884755ce9d56824b526327
Antonio0512 <antonioboyanov9@gmail.com>
2023-11-24 19:14:28 +0200

    Adding new features - Connecting get matches from current date with the feed component - Altering the match component's scss file

--- frontend/src/components/home/feed/Feed.js
+++ frontend/src/components/home/feed/Feed.js
@@ -1,12 +1,53 @@
 import "./feed.scss";
 
 import { Match } from "../../match/Match";
+import { useEffect, useState } from "react";
+import { getMatches } from "../../../services/matchesService";
+import { Pagination } from "../../pagination/Pagination";
 
 export const Feed = () => {
+    const [matchesData, setMatchesData] = useState({
+        matches: [],
+        pagination: {
+            page: 1,
+            items_per_page: 10,
+            total_pages: 1,
+        },
+    });
+
+    const fetchData = async (page) => {
+        try {
+            const currentDate = new Date().toISOString();
+            const matchesData = await getMatches(page, currentDate);
+            setMatchesData(matchesData);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    useEffect(() => {
+        fetchData(matchesData.pagination.page);
+    }, []);
+
+    const handlePageChange = (pageNumber) => {
+        fetchData(pageNumber);
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    };
+
     return (
         <div className="feed">
             <div className="feedWrapper">
-                <Match />
+                <div className="match">
+                    {matchesData?.matches.map((match) => (
+                        <Match match={match} key={match.id} />
+                    ))}
+                </div>
+                {matchesData?.matches.length > 0 && (
+                    <Pagination
+                        handlePageChange={handlePageChange}
+                        dataToFetch={matchesData}
+                    />
+                )}
             </div>
         </div>
     );
--- frontend/src/components/match/Match.js
+++ frontend/src/components/match/Match.js
@@ -1,102 +1,72 @@
+import { Link } from "react-router-dom";
 import "./match.scss";
+import { TOURNAMENTS } from "../../routes/routes";
 
-export const Match = () => {
+export const Match = ({ match }) => {
+    if (!match || !match.score || match.score.length < 2) {
+        return null;
+    }
+
+    const matchDate = new Date(match.date);
+
+    const dateOptions = {
+        year: "numeric",
+        month: "numeric",
+        day: "numeric",
+    };
+
+    const timeOptions = {
+        hour: "numeric",
+        minute: "numeric",
+        hour12: true,
+    };
+
+    const formattedDate = matchDate.toLocaleString(undefined, dateOptions);
+    const formattedTime = matchDate.toLocaleString(undefined, timeOptions);
+
     return (
-        <div className="match">
-            <div className="matchWrapper">
-                <div className="matchContainer">
-                    <div className="matchBox">
-                        <div className="matchHeader">
-                            <div className="matchHeaderLeft">
-                                <span>Live</span>
-                            </div>
-                            <div className="matchHeaderRight">
-                                <span>20 mins ago</span>
-                            </div>
-                        </div>
-                        <hr />
-                        <div className="matchMain">
-                            <span className="matchTitle">
-                                Champions league semi-final
-                            </span>
-                            <div className="matchTeam">
-                                <div className="teamInfo">
-                                    <img
-                                        src="/images/teams/manchester-city.png"
-                                        alt="team1"
-                                    />
-                                    <span className="Team1">
-                                        Machester City
-                                    </span>
-                                </div>
-                                <span className="teamScore">1</span>
-                            </div>
-                            <div className="matchTeam">
-                                <div className="teamInfo">
-                                    <img
-                                        src="/images/teams/liverpool.png"
-                                        alt="team2"
-                                    />
-                                    <span>Liverpool</span>
-                                </div>
-                                <span className="teamScore">0</span>
-                            </div>
-                            <span className="matchTime">
-                                Etihad Stadium, Manchester
-                            </span>
-                        </div>
-                        <hr />
-                        <div className="matchBottom">
-                            <button className="actionButton">Watch Live</button>
-                        </div>
-                    </div>
-
-                    <div className="matchBox">
-                        <div className="matchHeader">
-                            <div className="matchHeaderLeft">
-                                <span>Live</span>
-                            </div>
-                            <div className="matchHeaderRight">
-                                <span>20 mins ago</span>
-                            </div>
-                        </div>
-                        <hr />
-                        <div className="matchMain">
-                            <span className="matchTitle">
-                                Champions league semi-final
-                            </span>
-                            <div className="matchTeam">
-                                <div className="teamInfo">
-                                    <img
-                                        src="/images/teams/manchester-city.png"
-                                        alt="team1"
-                                    />
-                                    <span className="Team1">
-                                        Machester City
-                                    </span>
-                                </div>
-                                <span className="teamScore">1</span>
-                            </div>
-                            <div className="matchTeam">
-                                <div className="teamInfo">
-                                    <img
-                                        src="/images/teams/liverpool.png"
-                                        alt="team2"
-                                    />
-                                    <span>Liverpool</span>
-                                </div>
-                                <span className="teamScore">0</span>
-                            </div>
-                            <span className="matchTime">
-                                Etihad Stadium, Manchester
-                            </span>
-                        </div>
-                        <hr />
-                        <div className="matchBottom">
-                            <button className="actionButton">Watch Live</button>
-                        </div>
-                    </div>
+        <div className="matchBox">
+            <div className="matchHeader">
+                <div className="matchHeaderLeft">
+                    <span>Live</span>
                 </div>
+                <div className="matchHeaderRight">
+                    <span>
+                        {formattedDate}, {formattedTime}
+                    </span>
+                </div>
+            </div>
+            <hr />
+            <div className="matchMain">
+                <Link
+                    className="link"
+                    to={`${TOURNAMENTS}/${match?.tournament_id}`}
+                >
+                    <span className="matchTitle">
+                        {match?.tournament_title}
+                    </span>
+                </Link>
+                <div className="matchTeam">
+                    <div className="teamInfo">
+                        <img
+                            src="/images/teams/manchester-city.png"
+                            alt="team1"
+                        />
+                        <span className="Team1">{match?.score[0].player}</span>
+                    </div>
+                    <span className="teamScore">{match?.score[0].score}</span>
+                </div>
+                <div className="matchTeam">
+                    <div className="teamInfo">
+                        <img src="/images/teams/liverpool.png" alt="team2" />
+                        <span>{match?.score[1].player}</span>
+                    </div>
+                    <span className="teamScore">{match?.score[1].score}</span>
+                </div>
+            </div>
+            <hr />
+            <div className="matchBottom">
+                <button className="actionButton">Watch Live</button>
             </div>
         </div>
     );
