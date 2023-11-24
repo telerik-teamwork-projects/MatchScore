85e3aaacf432ba71b6c38e5a980d5e683c8ed181
Antonio0512 <antonioboyanov9@gmail.com>
2023-11-24 16:44:23 +0200

    Adding new features - Connecting the standings from the database with the frontend

--- frontend/src/components/tournaments/tournamentDetails/TournamentDetails.js
+++ frontend/src/components/tournaments/tournamentDetails/TournamentDetails.js
@@ -112,6 +112,7 @@ export const TournamentDetails = () => {
                     </div>
                 )}
             </div>
+
             {tournament?.format === "knockout" && (
                 <div>
                     <TournamentKnockoutTree
